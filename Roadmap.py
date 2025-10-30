3531e92f3dc0361a0c243d7b1b50122176b63580
VedantSingh-dev <singhvedant092@gmail.com>
2025-10-30 13:13:09 +0530

    Update Roadmap.py

--- Roadmap.py
+++ Roadmap.py
@@ -1,6 +1,5 @@
 import os
 import google.generativeai as genai
-from google.generativeai import types
 from dotenv import load_dotenv
 import markdown
 load_dotenv()
@@ -44,20 +43,17 @@ def markdown_to_html(markdown_text):
 
 
 def generate(data):
-    client = genai.Client(
-        api_key=os.environ.get("GEMINI_API_KEY"),
-    )
-
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=f"""{data}""",
-        config=types.GenerateContentConfig(
-            system_instruction="you are a Leetcode profile Analyser who analysis leetcode profiles and help user get better at dsa for interviews. You will receive prompt in a json format while will contain a user's leetcode profile data like[No of ques solved , contest history ,no of ques topic wise]etc. you have to analyse that data and give response which tells. Things that user am doing wrong or mistakes user am making how can user improve on that what are some good things about user profile etc then you have to give a 6 weeks roadmap which tells week wise what to do which question or topics to solve how to solve some good resources to look up eg, Neetcode 150 , striver etc. and last also tell the expected outcomes of that 6 weeks roadmap. You have to be very raw and honest",
-            thinking_config=types.ThinkingConfig(thinking_budget=0)
-        ),
+    genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
+    model = genai.GenerativeModel("gemini-2.5-flash")
+    response = model.generate_content(
+        f"""{data}""",
+        generation_config={
+            "system_instruction": "you are a Leetcode profile Analyser who analysis leetcode profiles and help user get better at dsa for interviews. You will receive prompt in a json format while will contain a user's leetcode profile data like[No of ques solved , contest history ,no of ques topic wise]etc. you have to analyse that data and give response which tells. Things that user am doing wrong or mistakes user am making how can user improve on that what are some good things about user profile etc then you have to give a 6 weeks roadmap which tells week wise what to do which question or topics to solve how to solve some good resources to look up eg, Neetcode 150 , striver etc. and last also tell the expected outcomes of that 6 weeks roadmap. You have to be very raw and honest",
+        }
     )
     markdown_text = response.text
     return markdown_to_html(markdown_text)
+
 
 
 
