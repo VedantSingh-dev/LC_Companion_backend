f3653618663a78a8538bb58997dcde50eee393a4
VedantSingh-dev <singhvedant092@gmail.com>
2025-11-01 20:13:52 +0530

    Add CORS middleware to FastAPI application
    Add CORS middleware to FastAPI application

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 
 import LC_Stats
 import Roadmap
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5500",  
+    "http://localhost:5500",
+    
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            
+    allow_credentials=True,
+    allow_methods=["*"],              
+    allow_headers=["*"]              
+)
 
 @app.get("/")
 def greet():
